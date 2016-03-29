8c2010b7b5d86b3ca50ad6586730180246d45c36
MEGApixel23 <megapixel23@ya.ru>
2016-03-29 15:33:44 +0300

    Games template added.

---
+++ resources/views/games/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+@section('content')
+    <div class="row">
+        <div class="col-md-12 game-container">
+            123
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 game-container">
+            123
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 game-container">
+            123
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 game-container">
+            123
+        </div>
+    </div>
+@stop
