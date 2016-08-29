3a0b79f9e00c5033f0a7372d2d7b57d4a8906521
Dmitry Boyko <dmitry@thebodva.com>
2016-08-29 11:57:19 +0300

    small routes refactoring

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,28 +23,28 @@
 */
 
 //routes for jwt-applications
-Route::group(["middleware" => ["api"]], function() {
+Route::group(["middleware" => ["api"], "prefix" => "api"], function() {
 
     Route::resource(
-        '/api/index', 
+        '/index',
         'AuthenticateController', 
         ['only' => ['index']]);
 
     Route::post(
-        '/api/auth', 
+        '/auth',
         'AuthenticateController@authenticate');
 
     Route::post(
-        '/api/signup', 
+        '/signup',
         'AuthenticateController@signup');
 
     Route::post(
-        '/api/fb/signin', 
+        '/fb/signin',
         'AuthenticateController@fbCallback');
 
     //restricted routes
     Route::group(
-        ["middleware" => ["jwt.auth"], "prefix" => "api"],
+        ["middleware" => ["jwt.auth"]],
         function() {
             Route::get(
                 '/users',
@@ -120,66 +120,11 @@ Route::group(["middleware" => ["api"]], function() {
 
 
 Route::group(['middleware' => ['web']], function () {
-    // Authentication routes...
-//    Route::get('/signin', 'Auth\AuthController@getLogin')->name('login');
-//    Route::post('/signin', 'Auth\AuthController@postLogin')->name('checkLogin');
-//    Route::get('/logout', 'Auth\AuthController@getLogout')->name('logout');
-//
-//    Route::get(
-//        '/fb/signin',
-//        'Auth\AuthController@redirectToProvider')
-//            ->name('fbRegister');
 
-//    Route::get(
-//        '/fb/signin/callback',
-//        'Auth\AuthController@handleProviderCallback')
-//            ->name('fbRegisterCheck');
-//
-//    // Registration routes...
-//    Route::get('/signup', 'Auth\AuthController@getRegister')
-//        ->name('register');
-//    Route::post('/signup', 'Auth\AuthController@postRegister')
-//        ->name('checkRegister');
-
-    // Users routes
     Route::get('/user/search', 'UserController@search');
-//    Route::get('/user/role', 'UserController@role');
-
-//    Route::get(
-//        '/user/me',
-//        [   'middleware' => ['auth'],
-//            'uses' => 'UserController@one',]);
-
-//    Route::post('/user/{id}/restore', [
-//        'middleware' => ['auth'],
-//        'uses' => 'UserController@restore',
-//    ]);
-
-//    Route::get('/users', 'UserController@index');
-//    Route::group([
-//        'middleware' => ['auth', 'active']],
-//        function() {
-//            Route::put('/user/{id}', 'UserController@update')->where('id', '[0-9]+');
-//            Route::post('/user/{id}/avatar', 'UserController@updateAvatar')->where('id', '[0-9]+');
-//            Route::delete('/user/{id}', 'UserController@delete')->where('id', '[0-9]+');
-//    });
 
     // Games routes
     Route::get('/', 'GameController@index')->name('home');
-//    Route::get('game/{id}', 'GameController@one')->where('id', '[0-9]+');
-//    Route::group(['middleware' => ['auth', 'active']], function () {
-//        Route::put('game/{id}', 'GameController@update')->where('id', '[0-9]+');
-//        Route::delete('game/{id}', 'GameController@delete')->where('id', '[0-9]+');
-//        Route::get('game/{id}/delete', 'GameController@delete')->where('id', '[0-9]+');
-//        Route::post('game/{game_id}/complain', 'ComplaintController@create')->where('id', '[0-9]+');
-//        Route::delete('game/{game_id}/complain', 'ComplaintController@delete')->where('id', '[0-9]+');
-//    });
-//    Route::group(['middleware' => ['auth']], function () {
-//        Route::post('game', 'GameController@create');
-//    });
-
-
-    
 
     // Chart routes
     Route::get('/chart', 'ChartController@index')->name('chart');
